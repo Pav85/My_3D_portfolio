d63a64b861ff7f6b0e32c6882e0dfeb46710c8ac
Pav85 <pawel.werbowy@gmail.com>
2023-04-05 21:01:01 +0100

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -25,6 +25,18 @@ const Navbar = () => {
           <img src={logo} alt='logo' className='w-9 h-9 object-contain' />
           <p className='text-white text-[18px] font-bold cursor-pointer'>Pawel <span className='sm:block hidden'>| Front-End Developer</span></p>
         </Link>
+        <ul className='list-none hidden sm:flex flex-row gap-10'>
+          {navLinks.map((link) => (
+            <li
+            key={link.id}
+            className={`${
+              active === link.title
+            }`}
+            >
+              <a href={`#${link.id}`}>{link.title}</a>
+            </li>
+          ))}
+        </ul>
       </div>
     </nav>
   )
