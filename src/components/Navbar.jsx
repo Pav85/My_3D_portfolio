a5cebf96ddfd3e00347fbfc7e6badfff9ea87fd0
Pav85 <pawel.werbowy@gmail.com>
2023-04-06 17:00:03 +0100

    Update Navbar.jsx
    navbar functional

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -24,7 +24,10 @@ const Navbar = () => {
         }}
         >
           <img src={logo} alt='logo' className='w-9 h-9 object-contain' />
-          <p className='text-white text-[18px] font-bold cursor-pointer'>Pawel <span className='sm:block hidden'>| Front-End Developer</span></p>
+          <p className='text-white text-[18px] font-bold cursor-pointer flex'>
+            Pawel&nbsp;
+            <span className='sm:block hidden'>| Front-End Developer</span>
+            </p>
         </Link>
         <ul className='list-none hidden sm:flex flex-row gap-10'>
           {navLinks.map((link) => (
