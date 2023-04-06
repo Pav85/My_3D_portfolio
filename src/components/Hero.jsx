dcfeead1efc35398af8042d7560f770821d55fd3
Pav85 <pawel.werbowy@gmail.com>
2023-04-06 22:16:48 +0100

    Update Hero.jsx
    hero text added

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,8 +11,15 @@ const Hero = () => {
          <div className="w-5 h-5 rounded-full bg-[#915eff]" />
          <div className="w-1 sm:h-80 h-40 violet-gradient" />
         </div>
-
         
+        <div>
+          <h1 className={`${styles.heroHeadText} text-white mt-6`}>Hi, I'm <span className="text-[#915eff]">Pawel</span></h1>
+          <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+                a front-end developer with a passion for creating beautiful and user-friendly websites. 
+                With a solid understanding of HTML, CSS, and JavaScript, 
+                I'm dedicated to building engaging and responsive designs that deliver exceptional user experiences.
+                </p>
+        </div>
       </div>
     </section>
   )
