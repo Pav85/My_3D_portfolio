ff580730385318594f709fde8066447431387d76
Pav85 <pawel.werbowy@gmail.com>
2023-04-08 10:19:30 +0100

    Update Computers.jsx

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -4,7 +4,7 @@ import { OrbitControls, Preload, useGLTF } from '@react-three/drei';
 
 import CanvasLoader from '../Loader'
 
-const Computers = ({isMobile}) => {
+const Computers = ({ isMobile }) => {
   const computer = useGLTF('./desktop_pc/scene.gltf')
 
   return (
@@ -37,7 +37,9 @@ const ComputersCanvas = () => {
 
   useEffect(() => {
     const mediaQuery = window.matchMedia('(max-width: 500px)');
-    setMobile(mediaQuery.matches);
+
+    setIsMobile(mediaQuery.matches);
+    
     const handleMediaQueryChange = (event) => {
       setIsMobile(event.matches); 
     }
