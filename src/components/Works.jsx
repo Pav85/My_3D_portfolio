33f2002d43a1d7e8bb46fb90b6904fcf5dba5d0f
Pav85 <pawel.werbowy@gmail.com>
2023-04-11 00:14:21 +0100

    Update Works.jsx

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -45,6 +45,14 @@ const ProjectCard = ({ index, name, description, tags, image, source_code_link }
             <h3 className="text-white font-bold text-[24px] text-center">{name}</h3>
             <p className="mt-2 text-secondary text-[14px] text-justify">{description}</p>
           </div>
+
+          <div className="mt-4 flex flex-wrap gap-2">
+            {tags.map((tag) => (
+              <p key={tag.name} className={`text-[14px] ${tag.color}`}>
+                #{tag.name}
+              </p>
+            ))}
+          </div>
         </Tilt>
       </motion.div>
     )
