4034d35c747413b6d00ec1f53c5511446b4c577c
Pav85 <pawel.werbowy@gmail.com>
2023-04-11 08:11:08 +0100

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -8,7 +8,16 @@ import { SectionWrapper } from "../hoc";
 import { slideIn } from "../utils/motion";
 
 const Contact = () => {
-  // const [form, section]
+  const formRef = useRef();
+  const [form, setForm] = useState({
+    name: '',
+    email: '',
+    message: '',
+  })
+  const [loading, setLoading] = useState(false);
+  
+  const handleChange = (e) => {};
+  const handleSubmit = (e) => {};
 
   return (
     <div>Contact</div>
