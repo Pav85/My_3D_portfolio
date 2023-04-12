aa340b9593ec790944d76951d5cb535ee052891e
Pav85 <pawel.werbowy@gmail.com>
2023-04-12 14:07:09 +0100

    computer position
    computer positioning

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,7 @@ import { ComputersCanvas } from "./canvas";
 
 const Hero = () => {
   return (
-    <section className="relative w-full h-screen mx-auto">
+    <section className="relative w-full h-[1000px] mx-auto">
       <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}>
         <div className="flex flex-col justify-center items-center mt-3">
          <div className="w-5 h-5 rounded-full bg-[#915eff]" />
@@ -21,7 +21,7 @@ const Hero = () => {
                 </p>
         </div>
        </div>
-       
+
        <ComputersCanvas />
 
         <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -9,25 +9,22 @@ const Computers = ({ isMobile }) => {
 
   return (
     <mesh>
-      <hemisphereLight
-        intensity={0.15}
-        groundColor="black" 
-        />
-        <pointLight intensity={1} />
-        <spotLight 
-        position={[-20, 50, 10]} 
+      <hemisphereLight intensity={0.15} groundColor='black' />
+      <spotLight
+        position={[-20, 50, 10]}
         angle={0.12}
         penumbra={1}
         intensity={1}
         castShadow
         shadow-mapSize={1024}
-        />
-        <primitive
+      />
+      <pointLight intensity={1} />
+      <primitive
         object={computer.scene}
-        scale={isMobile ? 0.7 : 0.75}
-        position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
+        scale={isMobile ? 0.5 : 0.6}
+        position={isMobile ? [0, -2, -1.75] : [0, -2.75, -1.25]}
         rotation={[-0.01, -0.2, -0.1]}
-        />
+      />
     </mesh>
   )
 }
@@ -55,18 +52,19 @@ const ComputersCanvas = () => {
     <Canvas
       frameloop='demand'
       shadows
+      dpr={[1, 2]}
       camera={{ position: [20, 3, 5], fov: 25 }}
       gl={{ preserveDrawingBuffer: true }}
     >
       <Suspense fallback={<CanvasLoader />}>
-       <OrbitControls 
-         enableZoom={false}
-         maxPolarAngle={Math.PI / 2}
-         minPolarAngle={Math.PI / 2} 
-         />
-       <Computers isMobile={isMobile} />
+        <OrbitControls
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+        />
+        <Computers isMobile={isMobile} />
       </Suspense>
-      
+
       <Preload all />
     </Canvas>
   )
